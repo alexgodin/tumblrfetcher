1ed6cc5e02a3479bbacfdd8f6c98d8b374c27f05
Alex Godin <alex@alexgodin.com>
2012-02-05 16:04:46 -0500

    add the script

---
+++ fetch.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require "curb"
+require 'json'
+
+$blogname=corgiaddict
+$api_key="8XdzoAFSKX74TRv243IBfozMeR7l85XuNbZ5JyXP4Cc6VoC6G4"
+
+def tumble(action, options="")
+  JSON.parse(Curl::Easy.perform("http://api.tumblr.com/v2/blog/#{$blogname}.tumblr.com/#{action}?api_key=#{$api_key}#{options}").body_str)
+end
+
+def number_of_posts
+  tumble("info")["response"]["blog"]["posts"]
+end
+
+def number_of_pages
+  number_of_posts/20
+end
+
+def get_photos
+  number_of_pages.times do |i|
+    tumble("posts/photo", "&offset=#{i*20}")["response"]["posts"].each do |item|
+      system "curl -O #{item["photos"][0]["alt_sizes"][1]["url"]}"
+    end
+  end
+end
+
+get_photos
